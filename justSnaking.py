7bcce0293e246e081c17ecf18379b32eac2aeef4
amro125 <amitrogel97@gmail.com>
2024-11-01 11:34:43 -0400

    Update justSnaking.py

--- justSnaking.py
+++ justSnaking.py
@@ -21,41 +21,40 @@ def robomove():
     direction = -1
     max = 20
 
-    speed = 0
+    # speed = 0
     face = 0
     add = 0
-    while True:
-        ### going up ###
-        tomove = []
+    ### going up ###
+    tomove = []
+    for xarm in xarms:
+        tomove.append(xarm.snakebeathalf(amps, speeds[speed], phases))
+    # end = float(traj[-1])
+    # IP = 1
+    for i in range(len(tomove[0])):
+        start = time.time()
+        num = 0
         for xarm in xarms:
-            tomove.append(xarm.snakebeathalf(amps, speeds[speed], phases))
-        # end = float(traj[-1])
-        # IP = 1
-        for i in range(len(tomove[0])):
-            start = time.time()
-            num = 0
-            for xarm in xarms:
-                pos = tomove[num][i].copy()
-                xarm.movexArm(pos)
-                num += 1
+            pos = tomove[num][i].copy()
+            xarm.movexArm(pos)
+            num += 1
+        t_elapse = time.time() - start
+        while t_elapse < tstep:
+            time.sleep(0.0001)
             t_elapse = time.time() - start
-            while t_elapse < tstep:
-                time.sleep(0.0001)
-                t_elapse = time.time() - start
 
-        ### going down ####
-        tomove = []
+    ### going down ####
+    tomove = []
+    for xarm in xarms:
+        tomove.append(xarm.snakebeathalf(amps, speeds[speed],[x+1 for x in phases]))
+    for i in range(len(tomove[0])):
+        start = time.time()
+        num = 0
         for xarm in xarms:
-            tomove.append(xarm.snakebeathalf(amps, speeds[speed],[x+1 for x in phases]))
-        for i in range(len(tomove[0])):
-            start = time.time()
-            num = 0
-            for xarm in xarms:
-                xarm.movexArm(tomove[num][i])
-                num += 1
-            t_elapse = time.time() - start
-            while t_elapse < tstep:
-                time.sleep(0.0001)
+            xarm.movexArm(tomove[num][i])
+            num += 1
+        t_elapse = time.time() - start
+        while t_elapse < tstep:
+            time.sleep(0.0001)
                 t_elapse = time.time() - start
 
 
@@ -80,6 +79,7 @@ xarms = []
 speeds = np.linspace(3, 0.5, 20)
 amps = [5, 0, 15, 0, 30, 0, 0]
 phases = [0, 0, 0.5, 0, 0.3, 0, 0]
+
 #be
 for robot in robots:
     xarms.append(medusaiutils.robotsUtils(robot[0], robot[1], sim=False))
@@ -90,7 +90,8 @@ for xarm in xarms:
     xarm.setupBot(IP[0])
 threading.Thread(target=robomove, daemon=True).start()
 input("press enter when robots stop moving to start script")
-robomove()
+while True:
+    robomove()
     # q.put(int(data.decode('utf-8')))
 
 
