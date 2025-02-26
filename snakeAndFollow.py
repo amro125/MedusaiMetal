8cab60603654853f206c695557f91cc11699509f
amro125 <amitrogel97@gmail.com>
2025-02-26 13:18:17 -0500

    Big Changes
    big things are coming

--- snakeAndFollow.py
+++ snakeAndFollow.py
@@ -9,28 +9,13 @@ import socket
 import medusaiutils
 
 
-# def snakebeat(duration,phase):
-#     t = np.arange(0,duration+tstep,tstep)
-#     print(t[-1])
-
-#     traj = [round(-0.5*math.cos((math.pi*(q-phase*duration))/duration)+0.5,4) for q in t]
-#     return traj
-
-def FollowUser():
-    # if q.qsize() > 0: # scary stop
-    x, y, pluck = q.queue[-1]
-    print(x, y, pluck)
-    # print("JFIOEWJFOIWEJFOIWEJFOWIEJFOIWEJF")
+def makefollowTraj(x,y):
+    #this is the follow stuff elaine made
+    xlimit = [[120, 330], [330, 475]]
+    j3 = [[-25, 67], [-170, -100]]
     tomove = []
     num = 0
-
-    # TODO: slow down snakebeat but check before
     for xarm in xarms:
-        robottrajirst = xarm.snakebeat1(amps, 2.5, phases)
-        robottraj = np.full((len(robottrajirst), len(robottrajirst[0])), robottrajirst[0])
-        # robottraj = robottrajirst[0]
-        # robottraj= robottraj*len(robottrajirst)
-
         if x <= xlimit[num][0]:
             movej3 = j3[num][0]
         elif x >= xlimit[num][1]:
@@ -44,20 +29,61 @@ def FollowUser():
         nextspot = curpos.copy()
         nextspot[2] = movej3
         print(curpos)
-        xarm.Singlep2ptraj
-        trajx = xarm.fifth_poly1(curpos[2], movej3, 0, 0, 2)
-        # curpos[2] = trajx[-1]
-        newrobottraj = robottraj[:len(trajx)]  # bc the remaining robottraj has empty j3 column
-        newrobottraj[:, 2] = trajx
-        print("LENGTHS", len(robottraj), len(newrobottraj))
-        tomove.append(newrobottraj)
+        curtraj = xarm.Singlep2ptraj(curpos, nextspot, 1.5)
+        # curtraj = xarm.fifth_poly1(curpos[2], movej3, 0, 0, 2)
+        tomove.append(curtraj)
+    return tomove
+
+
+def FollowUser():
+    #
+    xlimit = [[120, 330], [330, 475]]
+    j3 = [[-25, 67], [-170, -100]]
+
+    #small pause for suspense, can be longer or shorter
+    time.sleep(0.5)
+    # input("go to position?")
+    x, y, pluck = q.queue[-1]
+    print(x, y, pluck)
+    
+    #creates our follow trajectory
+    tomove = makefollowTraj(x,y)
+
+    for i in range(len(tomove[0])):
+        start = time.time()
+        num = 0
+        for xarm in xarms:
+            pos = tomove[num][i].copy()
+            xarm.movexArm(pos)
+            num += 1
+        t_elapse = time.time() - start
+        while t_elapse < tstep:
+            time.sleep(0.0001)
+            t_elapse = time.time() - start
     time.sleep(2)
+    #return to snake position
     tomove = []
     num = 0
     for xarm in xarms:
-        
+        robottrajirst = xarm.snakebeat1(amps, 5, phases)
+        curpos = xarm.getAngle1()
+        curtraj= xarm.Singlep2ptraj(curpos, robottrajirst[0], 1.5)
+        tomove.append(curtraj)
+    for i in range(len(tomove[0])):
+        start = time.time()
+        num = 0
+        for xarm in xarms:
+            pos = tomove[num][i].copy()
+            xarm.movexArm(pos)
+            num += 1
+        t_elapse = time.time() - start
+        while t_elapse < tstep:
+            time.sleep(0.0001)
+            t_elapse = time.time() - start
+    # input("good?")
+    #clear input so that we can reset snaking
     q.queue.clear()
-    
+
 
 
 def robomove():
@@ -73,63 +99,20 @@ def robomove():
     add = 0
     x = 150
     y = 0
-    # curpos = []
-    # for xarm in xarms:
-    #     curpos = xarm.getAngle()
+
 
     while True:
 
         ### going up ###
         tomove = []
         num = 0
-        # if q.qsize() > 0: # scary stop
-        #     x, y, pluck = q.queue[-1]
-        #     print(x, y, pluck)
-        #     # print("JFIOEWJFOIWEJFOIWEJFOWIEJFOIWEJF")
-        #
-        #     # TODO: slow down snakebeat but check before
-        #     for xarm in xarms:
-        #         robottrajirst = xarm.snakebeat1(amps, 2.5, phases)
-        #         robottraj = np.full((len(robottrajirst), len(robottrajirst[0])), robottrajirst[0])
-        #         # robottraj = robottrajirst[0]
-        #         # robottraj= robottraj*len(robottrajirst)
-        #
-        #         if x <= xlimit[num][0]:
-        #             movej3 = j3[num][0]
-        #         elif x >= xlimit[num][1]:
-        #             movej3 = j3[num][1]
-        #         else:
-        #             movej3 = ((x - xlimit[num][0]) / (xlimit[num][1] - xlimit[num][0]) * (j3[num][1] - j3[num][0])) + \
-        #                      j3[num][0]
-        #             # print("should stop", movej3)
-        #         num += 1
-        #         curpos = xarm.getAngle1()
-        #         print(curpos)
-        #         trajx = xarm.fifth_poly1(curpos[2], movej3, 0, 0, 2)
-        #         # curpos[2] = trajx[-1]
-        #         newrobottraj = robottraj[:len(trajx)]  # bc the remaining robottraj has empty j3 column
-        #         newrobottraj[:, 2] = trajx
-        #         print("LENGTHS", len(robottraj), len(newrobottraj))
-        #         tomove.append(newrobottraj)
-        #     q.queue.clear()
 
-        # else:
         for xarm in xarms:
-            robottraj = xarm.snakebeat1(amps, 2.5, phases)
+            #Just do any slow snake movement
+            robottraj = xarm.snakebeat1(amps, 5, phases)
 
-            # if x <= xlimit[num][0]:
-            #     movej3 = j3[num][0]
-            # elif x >= xlimit[num][1]:
-            #     movej3 = j3[num][1]
-            # else:
-            #     movej3 =((x - xlimit[num][0]) / (xlimit[num][1] - xlimit[num][0]) * (j3[num][1]-j3[num][0])) + j3[num][0]
-                # print("!Q!!!!!!!!!!!!!!!!!!!!!!!!!!!",movej3)
             num += 1
-            # curpos = xarm.getAngle1()
-            # print(curpos)
-            # trajx = xarm.fifth_poly1(curpos[2],movej3,0,0,2.5)
-            # curpos[2] = trajx[-1]
-            # robottraj[:,2] = trajx
+
             tomove.append(robottraj)
 
         # end = float(traj[-1])
@@ -147,9 +130,14 @@ def robomove():
                 xarm.movexArm(pos)
                 num += 1
             t_elapse = time.time() - start
+            #instead of checking for movement during generation, we check during execution
             if q.qsize() > 0:
                 print("STOP")
+                FollowUser()
                 q.queue.clear()
+                # cuts
+                break
+
             while t_elapse < tstep:
                 time.sleep(0.0001)
                 t_elapse = time.time() - start
